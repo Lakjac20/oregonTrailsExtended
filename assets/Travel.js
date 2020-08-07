add101e895a9ebd9bb189b571576ca9e819d741f
Lakwanjala <lakwanjalawilliams@gmail.com>
2020-08-07 10:38:52 -0500

    created 2 child classes and new methods

--- assets/Travel.js
+++ assets/Travel.js
@@ -17,3 +17,39 @@ class Traveler {
         }
     }
 }
+
+class Doctor extends Traveler {
+    constructor (name){
+        super(name)
+
+    }
+    heal(Traveler){
+        Traveler.isHealthy = true
+    }
+}
+
+class Hunter extends Traveler {
+    constructor (name){
+        super (name)
+        this.food = 2
+        
+    }
+    hunt() {
+        this.food = this.food + 5
+    }
+    eat () {
+        if (this.food >= 2) {
+            this.food = this.food - 2
+        } else {
+            this.food = 0
+            this.isHealthy = false
+        }
+    }
+    giveFood (Traveler, numOfFoodUnits) {
+        if (this.food >= numOfFoodUnits ){
+            this.food = this.food - numOfFoodUnits
+            Traveler.food = numOfFoodUnits + Traveler.food
+        }
+      
+    }
+}
